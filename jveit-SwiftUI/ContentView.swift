af21c1ee9fdcd10618073e62f1880b5a44f8701e
Jeffrey Veit <jveit1186@gmail.com>
2021-05-14 14:06:47 -0700

    disabling feature

--- jveit-SwiftUI/ContentView.swift
+++ jveit-SwiftUI/ContentView.swift
@@ -23,31 +23,28 @@ struct ContentView: View {
     @State private var sliderValue = 0.0
     @State private var stepperValue = 0
     @State private var hiddenIndex = 1
-    @State var disableIndex = 1
     @State var imageT = "tv"
+    @State var isActive = true
     
-    var hidden : Bool
-    {
-        hiddenIndex == 0
-    }
+    
     
     @State private var currentPage = Page.Remote
     
     var body: some View
     {
-        let switch1 =  Toggle("Switch", isOn: $switchOn).labelsHidden().disabled(disableIndex == 0)
-        let switch2 =  Toggle("Switch", isOn: $switchOn).labelsHidden().disabled(disableIndex == 0)
-        let slider = Slider(value: $sliderValue, in: 0...100).disabled(disableIndex == 0)
-        let stepper = Stepper(value: $stepperValue, in: 0...100, label: {Text("Channel:  \(stepperValue)").padding()})
+        let switch1 =  Toggle("Switch", isOn: $switchOn).labelsHidden()
+        let switch2 =  Toggle("Switch", isOn: $switchOn).labelsHidden().disabled(!switchOn)
+        let slider = Slider(value: $sliderValue, in: 0...100).disabled(!switchOn)
+        let stepper = Stepper(value: $stepperValue, in: 0...100, label: {Text("Channel:  \(stepperValue)").padding()}).disabled(!switchOn)
         let picker = Picker("Favorites", selection: $selectedNameIndex)
             {
                 ForEach(0 ..< channels.count)
                 {
                     i in Text(channels[i]).tag(i)
                 }
-            }.pickerStyle(SegmentedPickerStyle())
-            
-            ZStack
+            }.pickerStyle(SegmentedPickerStyle()).disabled(!switchOn)
+        
+        ZStack
         {
             if currentPage == .Remote
             {
